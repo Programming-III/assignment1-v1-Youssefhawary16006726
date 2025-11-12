121aaf920be8df81f637433e6f1721979b056ab2
Youssefhawary16006726 <yohawaryyo6@gmail.com>
2025-11-12 15:15:33 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -1,7 +1,17 @@
 #ifndef REPTILE_H
 #define REPTILE_H
 #include "Animal.h"
-
+class Reptile:public Animal{
+private:
+ bool isVenomous;
+public:
+Reptile();
+Reptile(venom);  
+~Reptile();        
+ void setisVenomous();
+  bool getisVenomous();
+   
+    };
 
 
 
