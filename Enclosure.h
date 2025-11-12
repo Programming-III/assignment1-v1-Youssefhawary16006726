e6b9391cf3400026f21a787f6cdb5462bd0940bf
Youssefhawary16006726 <yohawaryyo6@gmail.com>
2025-11-12 15:13:45 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -1,9 +1,22 @@
 #ifndef ENCLOSURE_H
 #define ENCLOSURE_H
-
 #include "Animal.h"
-
-//define enclosure class here 
+class Enclosure{
+private:
+Animal*animals;
+int capacity;
+int currentcount;
+public:
+void addAnimal(Animal*a);
+void displayAnimals();
+Enclosure();
+Enclosure(cap,countt,)
+void setcapacity();
+void setcurrentcount();
+int getcapacity();
+int getcurrentcount();
+~Enclosure();
+};
 
 
 
