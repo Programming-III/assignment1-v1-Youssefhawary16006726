0d09c204daabf22ea395f08f82d6f56f5d46fedc
Youssefhawary16006726 <yohawaryyo6@gmail.com>
2025-11-12 15:14:38 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,9 +1,17 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
 #include "Animal.h"
+class Mammal:public Animal{
+private:
+string furcolor;
+public:
+Mammal();
+Mammal(string fcolor);
+~Mammal();
+void setfurcolor();
+string getfurcolor();
+};
 
-//define mammal class here
 
 
 
